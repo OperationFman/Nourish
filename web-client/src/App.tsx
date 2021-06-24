6b6088cc3ff62945097d637e3a4b19f51638f7ad
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-24 15:53:09 +1000

    Added navbar template

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/*  Header with logo, filter and search component */}
+        <Navbar />
+        
       </header>
         {/* Authentication component */}
         {/* Feed component */}
---
+++ web-client/src/components/navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./Navbar.css"
+
+const Navbar = () => (
+  <nav className="navbar">
+    <div className="navbar--logo-holder">
+      <h1> Nourish </h1>
+    </div>
+    <ul className="navbar--link">
+      <li className="navbar--link-item">Search</li>
+      <li className="navbar--link-item">Sort</li>
+      <li className="navbar--link-item">Login</li>
+    </ul>
+  </nav>
+)
+export default Navbar
