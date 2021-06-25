c7d2d7da1e9e0f5f1f7e4245320e6687f7ff14c6
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-25 13:23:40 +1000

    Update App.tsx

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -3,13 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/*  Header with logo, filter and search component */}
+    // <div className="App">
+    //   <header className="App-header">
+    //     {/*  Header with logo, filter and search component */}
         
-      </header>
-        {/* Authentication component */}
-        {/* Feed component */}
+    //   </header>
+    //     {/* Authentication component */}
+    //     {/* Feed component */}
+    // </div>
+    <div className="grid-container">
+      <div className="grid-item grid-item-1" style={{border: "2px solid white"}}>1</div>
+      <div className="grid-item grid-item-2" style={{border: "2px solid white"}}>2</div>
+      <div className="grid-item grid-item-3" style={{border: "2px solid white"}}>3</div>
+      <div className="grid-item grid-item-4" style={{border: "2px solid white"}}>4</div>
+      <div className="grid-item grid-item-5" style={{border: "2px solid white"}}>5</div>
+      <div className="grid-item grid-item-6" style={{border: "2px solid white"}}>6</div>
+      <div className="grid-item grid-item-7" style={{border: "2px solid white"}}>7</div>
     </div>
   );
 }
