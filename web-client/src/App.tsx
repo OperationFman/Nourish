e2105ebaff2d02f16765ef8651b8f2eb8eb136b8
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-26 12:00:47 +1000

    Update App.tsx

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -11,19 +11,19 @@ function App() {
     //     {/* Authentication component */}
     //     {/* Feed component */}
     // </div>
-    <div className="grid-container">
-      <div className="card responsive-scale" style={{backgroundImage:`url(https://images.unsplash.com/photo-1598554869714-fffca33c3e67?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1210&q=80)`}}>1</div>
-      <div className="card responsive-scale">2</div>
-      <div className="card responsive-scale">3</div>
-      <div className="card responsive-scale">4</div>
-      <div className="card responsive-scale">5</div>
-      <div className="card responsive-scale">6</div>
-      <div className="card responsive-scale">7</div>
-      <div className="card responsive-scale">8</div>
-      <div className="card responsive-scale">9</div>
-      <div className="card responsive-scale">10</div>
-      <div className="card responsive-scale">11</div>
-      <div className="card responsive-scale">12</div>
+    <div className="grid-container animated-grid">
+      <div className="card responsive-scale" style={{backgroundImage:`url(https://images.unsplash.com/photo-1598554869714-fffca33c3e67?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1210&q=80)`}}></div>
+      <div className="card responsive-scale"></div>
+      <div className="card responsive-scale"></div>
+      <div className="card responsive-scale"></div>
+      <div className="card responsive-scale"></div>
+      <div className="card responsive-scale"></div>
+      <div className="card responsive-scale"></div>
+      <div className="card responsive-scale"></div>
+      <div className="card responsive-scale"></div>
+      <div className="card responsive-scale"></div>
+      <div className="card responsive-scale"></div>
+      <div className="card responsive-scale"></div>
     </div>
   );
 }
