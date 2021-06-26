adca6acdcbf0259a70d3e3c0da9918007ab179d5
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-26 20:27:48 +1000

    added dummy card

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Card from './components/feed/card';
+import DummyCard from './components/feed/dummy-card';
 
 function App() {
   return (
@@ -12,20 +13,22 @@ function App() {
     //     {/* Authentication component */}
     //     {/* Feed component */}
     // </div>
+    <React.Fragment>
+
+    <div className="temp-header">
+      <h1>Nourish</h1>
+    </div>
+
     <div className="grid-container animated-grid">
       <Card />
       <Card />
       <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <DummyCard />
+      <DummyCard />
+      <DummyCard />
+      
     </div>
+    </React.Fragment>
   );
 }
 
--- web-client/src/components/feed/card.tsx
+++ web-client/src/components/feed/card.tsx
@@ -8,14 +8,14 @@ class Card extends Component {
         const { } = this.props;
         
         return ( 
-            <div className="card responsive-scale" style={{backgroundImage:``}}>
+            <div className="card responsive-scale" style={{backgroundImage:`url(https://images.unsplash.com/photo-1551183053-bf91a1d81141?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1103&q=80)`}}>
                     <div className="card-wrapper">
                         <div className="card-title-price">
-                            <h3>Title</h3>
-                            <h3>$00.0</h3>
+                            <h3>Spaghetti</h3>
+                            <h3>$5.00</h3>
                         </div>
                         <div className="card-time">
-                            <h3>Time</h3>
+                            <h3>1h 30m 🕒</h3>
                         </div>
                     </div>
             </div>
