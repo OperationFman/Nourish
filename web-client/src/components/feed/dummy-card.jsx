2dabf29ea5e4265fc6998c8c0d2941cec6770f40
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-26 20:27:57 +1000

    fixed dummy card

---
+++ web-client/src/components/feed/dummy-card.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './feed.css';
+
+class DummyCard extends Component {
+    state = {  }
+
+    render() {
+        const { } = this.props;
+        
+        return ( 
+            <div className="card responsive-scale">
+                    <div className="dummy-card-wrapper">
+                        <div className="dummy-card-title">⠀</div>
+                        <div className="dummy-card-price">⠀</div>
+                        <div className="dummy-card-time">⠀</div>
+                    </div>
+            </div>
+        );
+    }
+}
+ 
+export default DummyCard;
