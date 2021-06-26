3836c2aa8b696d265c4672b965b6e6f1a04cdb5d
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-26 12:04:05 +1000

    Update App.tsx

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     //     {/* Feed component */}
     // </div>
     <div className="grid-container animated-grid">
-      <div className="card responsive-scale" style={{backgroundImage:`url(https://images.unsplash.com/photo-1598554869714-fffca33c3e67?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1210&q=80)`}}></div>
+      <div className="card responsive-scale" style={{backgroundImage:``}}></div>
       <div className="card responsive-scale"></div>
       <div className="card responsive-scale"></div>
       <div className="card responsive-scale"></div>
