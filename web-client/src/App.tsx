46a542f457668b7e94371b071fe5cda8993d7c0d
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-26 11:08:02 +1000

    Update App.tsx

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     //     {/* Feed component */}
     // </div>
     <div className="grid-container">
-      <div className="card">1</div>
+      <div className="card" style={{backgroundImage:`url(https://images.unsplash.com/photo-1598554869714-fffca33c3e67?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1210&q=80)`}}>1</div>
       <div className="card">2</div>
       <div className="card">3</div>
       <div className="card">4</div>
