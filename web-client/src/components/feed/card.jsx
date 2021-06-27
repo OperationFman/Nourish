a7f90da8a559e8e622ebbcdbfd4dd650a585cf2e
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-27 15:41:16 +1000

    Update card.jsx

--- web-client/src/components/feed/card.jsx
+++ web-client/src/components/feed/card.jsx
@@ -3,7 +3,7 @@ import './feed.css';
 
 const Card = props => {
     return ( 
-        <div className="card responsive-scale" style={{backgroundImage:`url(https://images.unsplash.com/photo-1551183053-bf91a1d81141?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1103&q=80)`}}>
+        <div className="card responsive-scale" style={{boxShadow: "inset 0px -50px 70px 0px hsla(0,0%,0%,.5)", backgroundImage:`url(https://images.unsplash.com/photo-1551183053-bf91a1d81141?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1103&q=80)`}}>
                 <div className="card-wrapper">
                     <div className="card-title-price">
                         <h3>Spaghetti</h3>
