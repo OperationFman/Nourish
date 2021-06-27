e639807e6d1ff728acbfb14dc67eda8787b14d8d
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-27 13:41:45 +1000

    Refactored dummy card into hook-based functional compoenent

--- web-client/src/components/feed/dummy-card.jsx
+++ web-client/src/components/feed/dummy-card.jsx
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './feed.css';
 
-class DummyCard extends Component {
-    state = {  }
-
-    render() {
-        const { } = this.props;
-        
-        return ( 
-            <div className="card responsive-scale">
-                    <div className="card-wrapper">
-                        <div className="dummy-card-title">⠀</div>
-                        <div className="dummy-card-price">⠀</div>
-                        <div className="dummy-card-time">⠀</div>
-                    </div>
+const DummyCard = props => {
+    return ( 
+        <div className="card responsive-scale">
+            <div className="card-wrapper">
+                <div className="dummy-card-title">⠀</div>
+                <div className="dummy-card-price">⠀</div>
+                <div className="dummy-card-time">⠀</div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
  
 export default DummyCard;
