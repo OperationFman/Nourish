3fd3abae07c88a7083f30cc95401106a3a009dc3
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-27 13:34:21 +1000

    Refactored redundant class

--- web-client/src/components/feed/dummy-card.jsx
+++ web-client/src/components/feed/dummy-card.jsx
@@ -9,7 +9,7 @@ class DummyCard extends Component {
         
         return ( 
             <div className="card responsive-scale">
-                    <div className="dummy-card-wrapper">
+                    <div className="card-wrapper">
                         <div className="dummy-card-title">⠀</div>
                         <div className="dummy-card-price">⠀</div>
                         <div className="dummy-card-time">⠀</div>
