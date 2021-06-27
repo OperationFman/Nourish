a55a1407bec03b3d1db43c7e66fdb041e8ee49d8
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-27 16:37:19 +1000

    Fixed header tag margin

--- web-client/src/components/authentication/login-button.jsx
+++ web-client/src/components/authentication/login-button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const LoginButton = () => {
   return (
     <button
-      className="login-button"
+      className="auth-button login-button"
       onClick={console.log('Login clicked')}
     >
     Log In
