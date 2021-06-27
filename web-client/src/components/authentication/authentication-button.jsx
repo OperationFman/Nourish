fb802636e11334b40b305757288b37dc371d55aa
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-27 15:51:14 +1000

    Create authentication-button.jsx

---
+++ web-client/src/components/authentication/authentication-button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginButton from "./login-button";
+import LogoutButton from "./logout-button";
+
+const AuthenticationButton = props => {
+    const isAuthenticated = false;
+
+    return isAuthenticated ? <LogoutButton /> : <LoginButton />;
+};
+
+export default AuthenticationButton;
