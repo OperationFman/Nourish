ac606440081787d75bffe9986289dc6fb7845f32
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-27 18:20:11 +1000

    Mopved sort into it's own folder because it will grow as a codebase

--- web-client/src/components/feed/search-sort/search-sort.jsx
+++ web-client/src/components/feed/search-sort/search-sort.jsx
@@ -1,10 +1,10 @@
 import Search from './search';
-import Sort from './sort';
+import Sort from './sort/sort';
 import './search-sort.css';
 
 const SearchSort = () => {
     return (
-        <div className="temp-search-sort-container">
+        <div className="search-sort-container">
             <Search />
             <Sort />   
         </div>
