9d3f3b25b4e9e4d5df145926ecbd47851806f293
Franklin Moon <franklin.m.moon@gmail.com>
2021-07-04 18:03:31 +1000

    Update App.js

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
-import SearchSort from './components/feed/search-sort/search-sort';
 import Navbar from './components/navbar/navbar';
+import SearchSort from './components/feed/search-sort/search-sort';
 import Feed from './components/feed/feed';
 import Recipe from './components/recipe/recipe';
 
