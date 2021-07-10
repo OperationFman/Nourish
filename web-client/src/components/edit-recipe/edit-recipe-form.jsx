57948e6334889fd0fdf6a303e57aeaecc583161d
Franklin Moon <franklin.m.moon@gmail.com>
2021-07-10 21:05:24 +1000

    Update edit-recipe-form.jsx

--- web-client/src/components/edit-recipe/edit-recipe-form.jsx
+++ web-client/src/components/edit-recipe/edit-recipe-form.jsx
@@ -1,11 +1,12 @@
 import { useForm, useFieldArray } from "react-hook-form";
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory  } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import './edit-recipe-form.css';
 
 const EditRecipe = () => {
   const location = useLocation();
   const { id } = location.state;
+  const history = useHistory();
   const authorId = 123456;
 
   const {
@@ -15,7 +16,7 @@ const EditRecipe = () => {
   } = useForm();
 
   const verifyFormData = (data) => {
-    if ('authorId' in data && 'id' in data && 'ingredients' in data && 'preparation' in data && 'hours' in data && 'minutes' in data && 'cost' in data && 'image' in data) {
+    if ('authorId' in data && 'id' in data && 'ingredients' in data && 'preparation' in data && 'hours' in data && 'minutes' in data && 'cost' in data) {
       return true
     }
     return false
@@ -25,8 +26,10 @@ const EditRecipe = () => {
     let result = {...data, id: id, authorId: authorId}
     result = !result.hours && {...result, hours: 0}
     const isFormValid = verifyFormData(result)
-    console.log(isFormValid);
-    console.log(result);
+    if (isFormValid) {
+      history.push("/");
+      console.log(result)
+    }
   };
 
   return (
@@ -35,25 +38,27 @@ const EditRecipe = () => {
         {console.log(id)}
 
           <label htmlFor="title">Recipe Title</label>
-          {errors?.title?.type === "required" && <p>Title is required</p>}
+          {errors?.title?.type === "required" && <p>Required</p>}
           <input id="title" defaultValue="" placeholder="" {...register("title", { required: true })} />
 
           <label htmlFor="ingredients">Ingredients </label>
+          {errors?.ingredients?.type === "required" && <p>Required</p>}
           <textarea id="ingredients" defaultValue="" placeholder="Separate by comma, e.g 12 eggs, 1L milk, bread, etc.." {...register("ingredients", { required: true })} /> 
 
           <label htmlFor="preparation"> Preparation Steps</label>
+          {errors?.preparation?.type === "required" && <p>Required</p>}
           <textarea id="preparation" defaultValue="" placeholder="" {...register("preparation", { required: true })} /> 
 
           <label htmlFor="hours">Hours to Prepare</label>
           <input type="number" id="hours" placeholder="0" name="hours" pattern="^-?[0-59]\d*\.?\d*$"  />
 
           <label htmlFor="minutes">Minutes to Prepare</label>
+          {errors?.minutes?.type === "required" && <p>Required</p>}
           <input type="number" id="minutes" placeholder="30" name="minutes" pattern="^-?[0-59]\d*\.?\d*$" {...register("minutes", { required: true })} />
 
           <label htmlFor="cost">Total Cost ($)</label>
           {errors?.cost?.type === "pattern" && <p>Cost should be whole number or a whole number + decimal. e.g 1, 1.0, 1.5, 0.6 etc</p>}
           <input id="cost" placeholder="5.00" name="cost" pattern="^-?[0-59]\d*\.?\d*$" {...register("cost", { required: true, pattern: '[+-]?([0-9]*[.])?[0-9]+' })}/>
-          {errors?.cost?.type === "required" && <p>Cost is required</p>}
 
           <label htmlFor="vegan"> Vegan </label>
           <input id='vegan' type="checkbox" {...register('vegan')} />
