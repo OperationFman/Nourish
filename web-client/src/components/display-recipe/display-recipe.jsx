e43813e9dff3ca9609cd3da9710200f874a61946
Franklin Moon <franklin.m.moon@gmail.com>
2021-07-11 18:39:05 +1000

    changes

--- web-client/src/components/display-recipe/display-recipe.jsx
+++ web-client/src/components/display-recipe/display-recipe.jsx
@@ -5,15 +5,9 @@ import './display-recipe.css';
 const Recipe = ({label}) => {
 
     return (
-        <>
         <div style={{width: '100px', background: 'grey'}}>
             <Link to='/'>Go Back</Link>
         </div>
-
-        <div data-testid="button">
-            {label}
-        </div>
-        </>
     )
 }
 
