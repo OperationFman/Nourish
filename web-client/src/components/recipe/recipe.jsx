3e76149a9b94e7e15299ca3b506f06cf54b49200
Franklin Moon <franklin.m.moon@gmail.com>
2021-07-11 14:26:42 +1000

    Update recipe.jsx

--- web-client/src/components/recipe/recipe.jsx
+++ web-client/src/components/recipe/recipe.jsx
@@ -1,86 +1,79 @@
-import { Link } from 'react-router-dom';
-import './recipe.css';
-import { useForm, useFieldArray } from "react-hook-form";
 import React from "react";
+import { useForm, useFieldArray } from "react-hook-form";
+import ReactDOM from "react-dom";
 
+import "./recipe.css";
 
-const Recipe = (props) => {
-    const { register, control, handleSubmit } = useForm({
-        defaultValues: {
-          food: [{ name: "pizza" }],
-          drinks: [{ name: "martini" }]
-        }
-      });
-    const {
+const Recipe = () => {
+  const { register, control, handleSubmit } = useForm({
+    defaultValues: {
+      food: [{ name: "pizza" }],
+      drinks: [{ name: "martini" }]
+    }
+  });
+  const {
     fields: foodFields,
     append: foodAppend,
     remove: foodRemove
-    } = useFieldArray({ control, name: "food" });
-    const {
+  } = useFieldArray({ control, name: "food" });
+  const {
     fields: drinkFields,
     append: drinkAppend,
     remove: drinkRemove
-    } = useFieldArray({ control, name: "drinks" });
+  } = useFieldArray({ control, name: "drinks" });
 
-    const onSubmit = data => console.log("data", data);
+  const onSubmit = data => console.log("data", data);
 
-    return (
-        <>
-        <div style={{width: '100px', background: 'grey'}}>
-            <Link to='/'>Go Back</Link>
-            <p> Sandbox to figure out Field Arrays</p>
-        </div>
-
-        <form onSubmit={handleSubmit(onSubmit)}>
-        <ul>
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <ul>
         <h5>FOOD</h5>
 
         {foodFields.map((item, index) => {
-            return (
+          return (
             <li key={item.id}>
-                <input name={`food[${index}].name`} {...register('foods')} />
+              <input {...register(`food.${index}.name`)} />
 
-                <button type="button" onClick={() => foodRemove(index)}>
+              <button type="button" onClick={() => foodRemove(index)}>
                 Delete
-                </button>
+              </button>
             </li>
-            );
+          );
         })}
         <button
-            type="button"
-            onClick={() => {
+          type="button"
+          onClick={() => {
             foodAppend({ name: "" });
-            }}
+          }}
         >
-            append food
+          append food
         </button>
-        </ul>
-        <ul>
+      </ul>
+      <ul>
         <h5>DRINKS</h5>
         {drinkFields.map((item, index) => {
-            return (
+          return (
             <li key={item.id}>
-                <input name={`drinks[${index}].name`} {...register('drink')} />
-                <button type="button" onClick={() => drinkRemove(index)}>
+              <input {...register(`drinks.${index}.name`)} />
+              <button type="button" onClick={() => drinkRemove(index)}>
                 Delete
-                </button>
+              </button>
             </li>
-            );
+          );
         })}
         <button
-            type="button"
-            onClick={() => {
+          type="button"
+          onClick={() => {
             drinkAppend({ name: "" });
-            }}
+          }}
         >
-            append drinks
+          append drinks
         </button>
-        </ul>
+      </ul>
 
-        <input type="submit" />
-        </form>
-    </>
-    )
+      <input type="submit" />
+    </form>
+  );
 }
 
 export default Recipe;
