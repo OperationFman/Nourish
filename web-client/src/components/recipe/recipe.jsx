cb65e86d1b32f8cb267486ecaa7f48dcae75077e
Franklin Moon <franklin.m.moon@gmail.com>
2021-07-11 14:44:37 +1000

    Update recipe.jsx

--- web-client/src/components/recipe/recipe.jsx
+++ web-client/src/components/recipe/recipe.jsx
@@ -1,50 +1,15 @@
-import { useForm, useFieldArray } from "react-hook-form";
+import { Link } from 'react-router-dom';
+import './recipe.css';
 
-import "./recipe.css";
 
-const Recipe = () => {
-  const { register, control, handleSubmit } = useForm({
-    defaultValues: {
-        ingredients: [{ name: "" }],
-        preparation: [{ name: "" }]
-    }
-  });
+const Recipe = (props) => {
 
-  const { fields: ingredientsFields, append: ingredientsAppend, remove: ingredientsRemove} = useFieldArray({ control, name: "ingredients" });
-  const { fields: preparationFields, append: preparationAppend, remove: preparationRemove} = useFieldArray({ control, name: "preparation" });
+    return (
+        <div style={{width: '100px', background: 'grey'}}>
+            <Link to='/'>Go Back</Link>
+        </div>
 
-  const onSubmit = data => console.log(data);
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-        
-        <ul>
-            {ingredientsFields.map((item, index) => {
-            return (
-                <li key={item.id}>
-                <input {...register(`ingredients.${index}.name`, { required: true })} />
-                <button type="button" onClick={() => ingredientsRemove(index)}> Delete </button>
-                </li>
-            );
-            })}
-            <button type="button" onClick={() => { ingredientsAppend({ name: "" }) }}> Add </button>
-        </ul>
-        
-        <ul>
-            {preparationFields.map((item, index) => {
-            return (
-                <li key={item.id}>
-                <input {...register(`preparation.${index}.name`, { required: true })} />
-                <button type="button" onClick={() => preparationRemove(index)}> Delete </button>
-                </li>
-            );
-            })}
-            <button type="button" onClick={() => { preparationAppend({ name: "" }) }}> Add </button>
-        </ul>
-
-      <input type="submit" />
-    </form>
-  );
+    )
 }
 
 export default Recipe;
