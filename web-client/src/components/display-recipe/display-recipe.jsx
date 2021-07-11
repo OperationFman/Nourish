242467333f8ac9239af11977bd9b273cab08c84d
Franklin Moon <franklin.m.moon@gmail.com>
2021-07-11 18:33:18 +1000

    havin a yarn

--- web-client/src/components/display-recipe/display-recipe.jsx
+++ web-client/src/components/display-recipe/display-recipe.jsx
@@ -2,13 +2,18 @@ import { Link } from 'react-router-dom';
 import './display-recipe.css';
 
 
-const Recipe = (props) => {
+const Recipe = ({label}) => {
 
     return (
+        <>
         <div style={{width: '100px', background: 'grey'}}>
             <Link to='/'>Go Back</Link>
         </div>
 
+        <div data-testid="button">
+            {label}
+        </div>
+        </>
     )
 }
 
