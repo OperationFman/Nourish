44efa79d194f8fc2d7763063e7d1fb96677d9861
Franklin Moon <franklin.m.moon@gmail.com>
2021-07-17 14:57:07 +1000

    Setup test suite

--- web-app/src/components/edit-recipe/__tests__/edit-recipe.test.jsx
+++ web-app/src/components/edit-recipe/__tests__/edit-recipe.test.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from "react-router-dom";
 import renderer from 'react-test-renderer';
-
 import EditRecipe from '../edit-recipe';
 
 
-test('render component', () => {
-    
-    render(
-        <MemoryRouter>
-            <EditRecipe />
-        </MemoryRouter>
-    );
-    expect(true).toBe(true); 
+describe('form elements initializing', () => {
+    beforeEach(() => {
+        render(
+            <MemoryRouter>
+                <EditRecipe />
+            </MemoryRouter>
+        );
+    })
+
+    afterEach(() => {
+        cleanup();
+    });
+
+    it('title input rendering', () => {
+        expect(true).toBe(true); 
+    })
 })
