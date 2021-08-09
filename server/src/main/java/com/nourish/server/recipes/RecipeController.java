cd8f5624686876894e4be69956889409e228f6a1
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2021-08-09 18:01:48 +1000

    Update RecipeController.java

--- server/src/main/java/com/nourish/server/recipes/RecipeController.java
+++ server/src/main/java/com/nourish/server/recipes/RecipeController.java
@@ -12,4 +12,92 @@ public class RecipeController {
     private boolean vegan;
     private boolean lactose;
     private String image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthorid() {
+        return authorid;
+    }
+
+    public void setAuthorid(String authorid) {
+        this.authorid = authorid;
+    }
+
+    public String[][] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String[][] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String[][] getPreparation() {
+        return preparation;
+    }
+
+    public void setPreparation(String[][] preparation) {
+        this.preparation = preparation;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public boolean isVegan() {
+        return vegan;
+    }
+
+    public void setVegan(boolean vegan) {
+        this.vegan = vegan;
+    }
+
+    public boolean isLactose() {
+        return lactose;
+    }
+
+    public void setLactose(boolean lactose) {
+        this.lactose = lactose;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
