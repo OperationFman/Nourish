bb67ed98bcbee96f468dac517a20eedd3d4d3007
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2021-08-09 18:00:18 +1000

    Create RecipeController.java

---
+++ server/src/main/java/com/nourish/server/recipes/RecipeController.java
@@ -0,0 +1,15 @@
+package com.nourish.server.recipes;
+
+public class RecipeController {
+    private String id;
+    private String title;
+    private String authorid;
+    private String[][] ingredients;
+    private String[][] preparation;
+    private int hours;
+    private int minutes;
+    private float cost;
+    private boolean vegan;
+    private boolean lactose;
+    private String image;
+}
