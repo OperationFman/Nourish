3fc328e43deb686ed419bc78513f70d35dce54fd
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2021-08-09 18:26:31 +1000

    Update RecipeController.java

--- server/src/main/java/com/nourish/server/recipes/RecipeController.java
+++ server/src/main/java/com/nourish/server/recipes/RecipeController.java
@@ -1,11 +1,20 @@
 package com.nourish.server.recipes;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class RecipeController {
 
-    @RequestMapping("/hello")
-    public String sayHi() {
-        return "Hi!";
+    @RequestMapping("/all-recipes")
+    public List<Recipe> getAllRecipes() {
+        return Arrays.asList(
+                new Recipe(1234, "Title1", 1234, null, null, 1, 30, 5.0, false, false, "n/a"),
+                new Recipe(5678, "Title2", 5678, null, null, 1, 30, 5.0, false, false, "n/a"),
+                new Recipe(9123, "Title3", 9123, null, null, 1, 30, 5.0, false, false, "n/a")
+        );
     }
 }
