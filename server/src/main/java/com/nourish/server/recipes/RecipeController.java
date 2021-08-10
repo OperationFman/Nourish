83497b9a477cb28aec395902e32e3c747045556a
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2021-08-10 14:01:53 +1000

    Update RecipeController.java

--- server/src/main/java/com/nourish/server/recipes/RecipeController.java
+++ server/src/main/java/com/nourish/server/recipes/RecipeController.java
@@ -3,16 +3,34 @@ package com.nourish.server.recipes;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RecipeController {
 
+    public class Test {
+        public int id;
+        public String name;
+
+        public Test(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
     @RequestMapping("/all-recipes")
     public List<Recipe> getAllRecipes() {
         String[] ingredients = {"carrot", "cheese", "bread"};
         String[] preparations = {"chop carrots", "grate cheese", "slice bread"};
+
+        ArrayList<Test> list = new ArrayList<Test>();
+        list.add(new Test(1,"bob"));
+        list.add(new Test(2,"steve"));
+
+
+
 
         return Arrays.asList(
                 new Recipe(1234, "Title1", 1234, ingredients, preparations, 1, 30, 5.0, false, false, "n/a"),
