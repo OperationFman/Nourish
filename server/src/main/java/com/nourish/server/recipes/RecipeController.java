9a8e350f6b41396014728c62eb6317c5970579a0
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2021-08-10 10:46:57 +1000

    Update RecipeController.java

--- server/src/main/java/com/nourish/server/recipes/RecipeController.java
+++ server/src/main/java/com/nourish/server/recipes/RecipeController.java
@@ -9,30 +9,15 @@ import java.util.List;
 @RestController
 public class RecipeController {
 
-public class Ingredient {
-    public int id;
-    public String name;
-
-    public Ingredient(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-}
-
     @RequestMapping("/all-recipes")
     public List<Recipe> getAllRecipes() {
-        List<Ingredient> = Arrays.asList(new Ingredient(0, "carot"),
-                                        new Ingredient(1, "cheese"),
-                                        new Ingredient(, "bread"));
+        String[] ingredients = {"carrot", "cheese", "bread"};
         String[] preparations = {"chop carrots", "grate cheese", "slice bread"};
 
-
-        System.out.println(ingredients[0].name);
-
         return Arrays.asList(
-                new Recipe(1234, "Title1", 1234, null, null, 1, 30, 5.0, false, false, "n/a"),
-                new Recipe(5678, "Title2", 5678, null, null, 1, 30, 4.0, false, false, "n/a"),
-                new Recipe(9123, "Title3", 9123, null, null, 1, 30, 3.5, false, false, "n/a")
+                new Recipe(1234, "Title1", 1234, ingredients, preparations, 1, 30, 5.0, false, false, "n/a"),
+                new Recipe(5678, "Title2", 5678, ingredients, preparations, 1, 30, 4.0, false, false, "n/a"),
+                new Recipe(9123, "Title3", 9123, ingredients, preparations, 1, 30, 3.5, false, false, "n/a")
         );
     }
 }
